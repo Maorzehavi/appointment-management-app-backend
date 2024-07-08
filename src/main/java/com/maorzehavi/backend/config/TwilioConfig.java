174b87aa94777f0662e7d33eb6f47afc7da7d4c9
Maorz <maorzehavi@outlook.com>
2024-07-08 10:56:16 +0300

    continue twilio config

--- src/main/java/com/maorzehavi/backend/config/TwilioConfig.java
+++ src/main/java/com/maorzehavi/backend/config/TwilioConfig.java
@@ -3,32 +3,21 @@ package com.maorzehavi.backend.config;
 import com.twilio.Twilio;
 import jakarta.annotation.PostConstruct;
 import lombok.Data;
-import lombok.extern.log4j.Log4j;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
+import lombok.Getter;
 import org.springframework.context.annotation.Configuration;
-
 
 
 @Configuration
 @Data
 public class TwilioConfig {
+    @Getter
+    private static final String ACCOUNT_SID = System.getenv("TWILIO_ACCOUNT_SID");
+    @Getter
+    private static final String AUTH_TOKEN = System.getenv("TWILIO_AUTH_TOKEN");
 
-    private static final String ACCOUNT_SID = System.getenv("TWILIO_ACCOUNT");
-    private static final String AUTH_TOKEN = System.getenv("TWILIO_AUTH");
-
-
-
-
-
-    @Value("${twilio.accountSid}")
-    private String accountSid;
-
-    @Value("${twilio.authToken}")
-    private String authToken;
 
     @PostConstruct
     public void init() {
-        Twilio.init(accountSid, authToken);
+        Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
     }
 }
--- src/test/java/com/maorzehavi/backend/BackendApplicationTests.java
+++ src/test/java/com/maorzehavi/backend/BackendApplicationTests.java
@@ -1,13 +1,19 @@
 package com.maorzehavi.backend;
 
+import com.maorzehavi.backend.config.TwilioConfig;
+import com.twilio.rest.api.v2010.account.Message;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class BackendApplicationTests {
 
-    @Test
+
     void contextLoads() {
+
+
+
     }
 
 }
